3d474d0040683a93c31e37ed048239c1621b68d1
Irene Yang <ireneyang112@gmail.com>
2019-05-08 01:16:00 -0700

    changed layout paramener for setContentView, still doesn't work :''''(

--- app/src/main/java/com/example/happyhealthyme/MainActivity.java
+++ app/src/main/java/com/example/happyhealthyme/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+        setContentView(R.layout.activity_main);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
